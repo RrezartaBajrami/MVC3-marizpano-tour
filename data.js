53a52f13e848e2f547a9de98117767d39189fbc3
RrezartaBajrami <rrezartabajrami8@gmail.com>
2025-06-16 18:22:49 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,146 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-2",
+      "name": "360 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.658561983004752,
+          "pitch": 0.03432570822128511,
+          "rotation": 0,
+          "target": "1-360-3"
+        },
+        {
+          "yaw": 1.6561898344448833,
+          "pitch": 0.045570582563676965,
+          "rotation": 0,
+          "target": "2-360-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6631479916230525,
+          "pitch": 0.10770693736465375,
+          "title": "Bedroom 1",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6440526338924428,
+          "pitch": 0.12392673925879194,
+          "title": "Living room and kitchen",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-360-3",
+      "name": "360 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.665470650376097,
+          "pitch": 0.11850010923452814,
+          "rotation": 0,
+          "target": "0-360-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6661421986442262,
+          "pitch": 0.20216348128459138,
+          "title": "Hall",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-360-1",
+      "name": "360 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.9305026815465798,
+        "pitch": 0.16523777109437,
+        "fov": 1.4435897041262007
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9305026815465798,
+          "pitch": 0.16523777109437,
+          "rotation": 0,
+          "target": "0-360-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9305030423098808,
+          "pitch": 0.2378057073962836,
+          "title": "Hall",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
